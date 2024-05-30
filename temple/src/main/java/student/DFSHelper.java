d1f6ed43aef2b58b500dfc76a3f2d42c7585af97
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-05-30 16:06:57 +0200

    Refactor DFSHelper.java for readability and efficiency
    Simplified the code in the DFSHelper.java file by streamlining methods convertNeighboursToLongs and amIStuck. The refactor removes the need for intermediate collections and explicit loop by making use of stream operations. I find this a little more confusing but best way to get comfortable with streams is to use them.

--- temple/src/main/java/student/DFSHelper.java
+++ temple/src/main/java/student/DFSHelper.java
@@ -3,9 +3,7 @@ package student;
 import game.ExplorationState;
 import game.NodeStatus;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,18 +11,9 @@ import java.util.stream.Collectors;
 public class DFSHelper {
 
     public static Collection<Long> convertNeighboursToLongs(ExplorationState state){
-        Collection<NodeStatus> NodeStatusNeighbours = state.getNeighbours();
-        return NodeStatusNeighbours.stream().map(NodeStatus::nodeID).collect(Collectors.toList());
+        return state.getNeighbours().stream().map(NodeStatus::nodeID).collect(Collectors.toList());
     }
     public static boolean amIStuck(ExplorationState state, Set<Long> visited){
-        boolean amIStuck = true;
-        Collection<Long> neighbours = convertNeighboursToLongs(state);
-        for (Long neighbour : neighbours){
-            if (!visited.contains(neighbour)) {
-                amIStuck = false;
-                break;
-            }
-        }
-        return amIStuck;
+        return state.getNeighbours().stream().map(NodeStatus::nodeID).allMatch(visited::contains);
     }
 }
